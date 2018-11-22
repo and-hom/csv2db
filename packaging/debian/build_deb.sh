a26863665b3b6a1d75e08c815ed13fc0ee268d19
Андрей Хомяков <and.hom@gmail.com>
2018-11-22 23:25:04 +0300

    build script fix

--- packaging/debian/build_deb.sh
+++ packaging/debian/build_deb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # It's not the best way to build golang source package, but I don't know another
+rm -rf obj-x86_64-linux-gnu
 GOPATH=$(pwd)/obj-x86_64-linux-gnu/ go get github.com/and-hom/csv2db
 debuild -S
